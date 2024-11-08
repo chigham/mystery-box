31a787582a20d5d932c136af22828dab2334eb4b
Christopher Higham <christopher.a.higham@nasa.gov>
2024-11-08 10:43:27 -0500

    Add API call to the button. Test by clicking it.

--- static/script.js
+++ static/script.js
@@ -1,10 +1,32 @@
 const title = document.querySelector("h1");
 const titleButton = document.getElementById("title-button");
 
+const apiUrl = "https://health.aws.amazon.com/public/currentevents";
+
 titleButton.addEventListener("click", () => {
     if (title.style.color === "purple") {
         title.style.color = "red";
     } else {
         title.style.color = "purple";
     }
+    
+    let result = callAwsApi();
 });
+
+async function callAwsApi () {
+    await fetch(apiUrl, {mode: "no-cors"})  // cors fails, no-cors "works" (200) but returns opaque result
+        .then(response => {
+            console.log("made it this far");
+            // Result is not ok if mode is cors (default)
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.headers.values;//.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
